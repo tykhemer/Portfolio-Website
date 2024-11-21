024d5e0e488d35f85ef01da3219cdb7942fdf6bd
Jesus M <tykhemer@gmail.com>
2024-11-21 16:25:31 -0500

    Uhh animation yayyayayayayY!!
    Working on the animation yayayayayay!! woo hoo :D

---
+++ Work_Folder/hobbies.js
@@ -0,0 +1,7 @@
+const button=document.getElementById("back");
+
+button.addEventListener("click", function() {
+    window.location.href = "index.html";
+});
+
+
--- Work_Folder/home_page/hobbies.js
+++
@@ -1,5 +0,0 @@
-const back= document.getElementById('back');
-
-myButton.addEventListener('click', function() {
-console.log ("The button got pressed yayyyy!!");
-});
